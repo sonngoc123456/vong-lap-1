51609a3f11f6479de52d938ca08d9c2b69195cb3
Your Name <you@example.com>
2022-06-13 09:09:16 +0700

    vong lap file

---
+++ vong-lap-1.js
@@ -0,0 +1,7 @@
+let i;
+
+for(i = 0; i<=5; i++) {
+   let text = 'the number is ' + i + '<br>';
+    document.write(text)
+}
+
---
+++ vong-lap-for-2.js
@@ -0,0 +1,7 @@
+let number = prompt('enter your number');
+let total = 0;
+let i;
+for(i = 1;i <= number;i+=1) {
+total = total + i;
+}
+alert(total);
